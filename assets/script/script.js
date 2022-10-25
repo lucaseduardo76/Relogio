78cf5ec6f356c957b63f14e0d2ae1243690cbd87
Lucas Eduardo Silva <99747569+lucaseduardo76@users.noreply.github.com>
2022-10-25 14:08:07 -0300

    Aprimorando interface

--- assets/script/script.js
+++ assets/script/script.js
@@ -14,17 +14,11 @@ const count = ()=>{
     }
     document.querySelector('.digital').innerHTML = `${hours}:${minutes}:${seconds}`;
 
-
-
     document.querySelector('.p_s').style.transform = `rotate(${-90 + seconds * 6}deg)`
-    document.querySelector('.p_m').style.transform = `rotate(${-90 + minutes * 6}deg)`
-
-    if(hours > 12){
-        hours = hours - 12
-    }
-
+    document.querySelector('.p_m').style.transform = `rotate(${(-90 + minutes * 6) + (seconds * 0.1)}deg)`
+    if(hours > 12){hours = hours - 12}
     document.querySelector('.p_h').style.transform = `rotate(${(-90 + hours * 30) + (minutes * 0.5)}deg)`
 }
 
-setInterval(count, 1000)
+let timer = setInterval(count, 1000)
 count()
