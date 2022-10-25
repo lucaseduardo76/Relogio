32ad6c5d807af09300cc26ea905519e240f9d026
Lucas Eduardo Silva <99747569+lucaseduardo76@users.noreply.github.com>
2022-10-25 14:45:25 -0300

    Aprimorando codigo

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,23 +2,22 @@ const count = ()=>{
     let date = new Date
     let seconds = date.getSeconds();
     let minutes = date.getMinutes();
-    let hours = date.getHours();
-    if(seconds < 10){
-        seconds = `0${seconds}`;
-    }
-    if(minutes < 10){
-        minutes = `0${minutes}`;
-    }
-    if(hours < 10){
-        hours = `0${hours}`;
-    }
-    document.querySelector('.digital').innerHTML = `${hours}:${minutes}:${seconds}`;
+    let hours = date.getHours();  
 
     document.querySelector('.p_s').style.transform = `rotate(${-90 + seconds * 6}deg)`
     document.querySelector('.p_m').style.transform = `rotate(${(-90 + minutes * 6) + (seconds * 0.1)}deg)`
     if(hours > 12){hours = hours - 12}
     document.querySelector('.p_h').style.transform = `rotate(${(-90 + hours * 30) + (minutes * 0.5)}deg)`
+    
+    document.querySelector('.digital').innerHTML = `${decNumb(hours)}:${decNumb(minutes)}:${decNumb(seconds)}`;
 }
 
+const decNumb = (num) =>{
+    if(num < 10){
+        return `0${num}`
+    }else{
+        return num 
+    }
+}
 let timer = setInterval(count, 1000)
 count()
