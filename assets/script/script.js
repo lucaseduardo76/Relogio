968b5fb63c4981ec2d97407d0c8e0f0908d3aff3
Lucas Eduardo Silva <99747569+lucaseduardo76@users.noreply.github.com>
2022-10-25 16:14:28 -0300

    Update script.js

--- assets/script/script.js
+++ assets/script/script.js
@@ -3,13 +3,13 @@ const count = ()=>{
     let seconds = date.getSeconds();
     let minutes = date.getMinutes();
     let hours = date.getHours();  
-
+    
+    document.querySelector('.digital').innerHTML = `${decNumb(hours)}:${decNumb(minutes)}:${decNumb(seconds)}`;
+    
     document.querySelector('.p_s').style.transform = `rotate(${-90 + seconds * 6}deg)`
     document.querySelector('.p_m').style.transform = `rotate(${(-90 + minutes * 6) + (seconds * 0.1)}deg)`
     if(hours > 12){hours = hours - 12}
-    document.querySelector('.p_h').style.transform = `rotate(${(-90 + hours * 30) + (minutes * 0.5)}deg)`
-    
-    document.querySelector('.digital').innerHTML = `${decNumb(hours)}:${decNumb(minutes)}:${decNumb(seconds)}`;
+    document.querySelector('.p_h').style.transform = `rotate(${(-90 + hours * 30) + (minutes * 0.5)}deg)`   
 }
 
 const decNumb = (num) =>{
