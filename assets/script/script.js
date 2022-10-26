9447546d8f10655ebaf7442200953e19655797e9
Lucas Eduardo <lesds2001@gmail.com>
2022-10-25 21:00:08 -0300

    Segundos linear

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,15 +1,16 @@
 const count = ()=>{
     let date = new Date
+    let milisecond = date.getMilliseconds();
     let seconds = date.getSeconds();
     let minutes = date.getMinutes();
     let hours = date.getHours();  
     
     document.querySelector('.digital').innerHTML = `${decNumb(hours)}:${decNumb(minutes)}:${decNumb(seconds)}`;
     
-    document.querySelector('.p_s').style.transform = `rotate(${-90 + seconds * 6}deg)`
-    document.querySelector('.p_m').style.transform = `rotate(${(-90 + minutes * 6) + (seconds * 0.1)}deg)`
+    document.querySelector('.p_s').style.transform = `rotate(${(-90 + seconds * 6) + (milisecond * (6/1000))}deg)`
+    document.querySelector('.p_m').style.transform = `rotate(${(-90 + minutes * 6) + (seconds * (1/10))}deg)`
     if(hours > 12){hours = hours - 12}
-    document.querySelector('.p_h').style.transform = `rotate(${(-90 + hours * 30) + (minutes * 0.5)}deg)`   
+    document.querySelector('.p_h').style.transform = `rotate(${(-90 + hours * 30) + (minutes * (5/10))}deg)`   
 }
 
 const decNumb = (num) =>{
@@ -19,5 +20,5 @@ const decNumb = (num) =>{
         return num 
     }
 }
-let timer = setInterval(count, 1000)
+let timer = setInterval(count, 50)
 count()
